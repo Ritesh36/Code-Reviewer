dd78fa60f7666f25c5c24f14272f4ce52d9e6d35
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-06-27 12:32:49 +0530

    completed frontend part

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes');
+const cors = require("cors");
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Welcome to the AI API!");
